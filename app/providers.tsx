47e67f625a902dab5c64b3d50aac6e9d288b14f7
bezzo.rx <bezzo19@gmx.de>
2025-08-30 13:16:31 +0200

    providers.tsx erstellen

---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { WalletAdapterProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useMemo } from 'react';
+import('@solana/wallet-adapter-react-ui/styles.css');
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  
+  return (
+    <WalletAdapterProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        {children}
+      </WalletModalProvider>
+    </WalletAdapterProvider>
+  );
+}
